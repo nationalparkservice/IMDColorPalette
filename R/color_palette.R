7596582125eb0d1fba58457fa7c23e10f0614048
Sarah E. Wright <s.wright.618@gmail.com>
2020-11-06 14:43:54 -0800

    Add color list and im_cols function

---
+++ R/color_palette.R
@@ -0,0 +1,136 @@
+im_colors <- c(
+  `MOJN RED L` = "#C78080",
+  `MOJN RED` = "#8F0000",
+  `MOJN RED D` = "#380000",
+  `MOJN DBROWN L` = "#BCA389",
+  `MOJN DBROWN` = "#784613",
+  `MOJN DBROWN D` = "#301C07",
+  `MOJN LBROWN L` = "#D3AA80",
+  `MOJN LBROWN` = "#A65400",
+  `MOJN LBROWN D` = "#422100",
+  `MOJN GOLD L` = "#D7CA80",
+  `MOJN GOLD` = "#AE9400",
+  `MOJN GOLD D` = "#453B00",
+  `MOJN GREEN L` = "#B6C097",
+  `MOJN GREEN` = "#6C8A2F",
+  `MOJN GREEN D` = "#2B3312",
+  `MOJN BLUE L` = "#B7E7FA",
+  `MOJN BLUE` = "#6ECEF4",
+  `MOJN BLUE D` = "#2C5261",
+  `MOJN WHITE L` = "#FFFFFF",
+  `MOJN WHITE` = "#DDDDDD",
+  `MOJN WHITE D` = "#BBBBBB",
+  `MOJN GRAY L` = "#D0D0D0",
+  `MOJN GRAY` = "#A1A1A1",
+  `MOJN GRAY D` = "#404040",
+  `MOJN BLACK L` = "#808080",
+  `MOJN BLACK` = "#222222",
+  `MOJN BLACK D` = "#000000",
+  `NPS BLUE` = "#BAE5F9",
+  `NPS ORANGE` = "#C56C39",
+  `NPS BROWN` = "#6F4930",
+  `NPS GREEN` = "#56903A",
+  `NPS BROWN D` = "#213A1B",
+  `DULL PINK` = "#6F5159",
+  `WARM GRAY` = "#ACA9A2",
+  `SPRING BLUE` = "#5585A9",
+  `AQUA` = "#9BDEEF",
+  `RICH BROWN` = "#385424",
+  `EARTH GREEN` = "#2D782C",
+  `PALE BROWN` = "#FAC9A1",
+  `TAN BROWN` = "#FDAA42",
+  `SIMPLE BROWN` = "#E95B0F",
+  `DEEP AQUA` = "#4AB0F2",
+  `MARINE BLUE` = "#013040",
+  `BRICK RED` = "#B42C02",
+  `SEA FOAM` = "#CCEBF0",
+  `DUSTY BROWN` = "#C0AA81",
+  `KHAKI` = "#AFC871",
+  `DARK AQUA` = "#3076B1",
+  `DULL BLUE` = "#234088",
+  `DULL BROWN` = "#32332D",
+  `DULL RED` = "#BF2E0F",
+  `BROWN` = "#BB853F",
+  `COOL BROWN` = "#C7BEAF",
+  `BLUE GRAY` = "#414550",
+  `DULL AQUA` = "#7FA2C0",
+  `FLAT AQUA` = "#57C9DD",
+  `MAGENTA` = "#B45090",
+  `PURPLE` = "#C69CD0",
+  `RICH GREEN` = "#6B8637",
+  `GREEN` = "#59AB47",
+  `BLUE GREEN` = "#81ADD2",
+  `DULL YELLOW` = "#E1E100",
+  `PALE AQUA` = "#DEE4F0",
+  `LIGHT BLUE` = "#679CD0",
+  `FLAT BLUE` = "#596980",
+  `OCEAN BLUE` = "#003C70",
+  `FLAT BROWN` = "#80815A",
+  `SILT BROWN` = "#806645",
+  `FADED BLUE` = "#6899BA",
+  `LIGHT ROSE` = "#DDCCBA",
+  `ROSE` = "#CC7667",
+  `FAR SHADOW` = "#432233",
+  `BLUE SLATE` = "#8C8F9E",
+  `DEEP BEIGE` = "#B8B397",
+  `WARM BEIGE` = "#F6D2B8",
+  `BEIGE` = "#F6BA90",
+  `STOPSIGN RED` = "#FF6E42",
+  `MUDDY BROWN` = "#9B5B4F",
+  `RED CLAY` = "#8F5F39",
+  `DEEP CAVE` = "#303129",
+  `BRIGHT BEIGE` = "#FFCCB1",
+  `SKY BLUE` = "#99CFFE",
+  `GREEN STONE` = "#7FAA9F",
+  `DRY GREEN` = "#A6B683",
+  `SHRUB BARK` = "#869463",
+  `PINE NEEDLE` = "#395137",
+  `BROWN GRAY` = "#867A6E",
+  `WET BARK` = "#587040",
+  `FOGGY LEAF` = "#99C383",
+  `FOREST MIST` = "#9DDFDE",
+  `SHADED MIST` = "#4D6160",
+  `FOREST STREAM` = "#303944",
+  `SANDY HILL` = "#FCEAA0",
+  `RAINCLOUD` = "#A6B4BF",
+  `DIRT` = "#928C2E",
+  `BLUE RIVER` = "#3B7492",
+  `RED BROWN` = "#A56621",
+  `DEEP RIVER` = "#475965",
+  `DARK LEAF` = "#548F01",
+  `FOREST BROWN` = "#B5B016",
+  `REDWOOD` = "#8B2F00",
+  `CONIFER` = "#40663F",
+  `DRY MEADOW` = "#DDE02B",
+  `DAMP MOSS` = "#848B64",
+  `CLOUD` = "#EBECF0",
+  `RIVER` = "#5B659F",
+  `LAKE` = "#01479D",
+  `DRY SHRUB` = "#D58A60",
+  `DRY HAY` = "#FFC283",
+  `WET SOIL` = "#5A6441",
+  `ORANGE` = "#F7941D"
+)
+
+
+#' Function to extract colors as hex codes
+#'
+#' @param ... Character names of colors
+#'
+#' @return A named character vector of hex codes
+#' @export
+#'
+#' @examples
+#' im_cols("RICH BROWN")
+#' im_cols("EARTH GREEN", "PALE BROWN")
+#'
+im_cols <- function(...) {
+  cols <- c(...)
+
+  if (is.null(cols)) {
+    return(im_colors)
+  } else {
+    return(im_colors[cols])
+  }
+}
+
